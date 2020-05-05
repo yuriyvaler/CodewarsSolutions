c8ff295793fda95f946feefabfc1ee36a24dda5e
Iurii Babarin <yuriyvaler@gmail.com>
2020-05-05 13:54:06 -0400

    Add solution

---
+++ position.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/find-the-position
+function position(letter){
+    let pos ='abcdefghijklmnopqrstuvwxyz';
+    return `Position of alphabet: ${pos.indexOf(letter)+1}`;
+}
