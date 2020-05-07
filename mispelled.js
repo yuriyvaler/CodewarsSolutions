bf09764d89d65850bbed0fbb4df2de074392a673
Iurii Babarin <yuriyvaler@gmail.com>
2020-05-07 12:00:55 -0400

    Add solution

--- mispelled.js
+++
@@ -1,9 +0,0 @@
-//https://www.codewars.com/kata/5892595f190ca40ad0000095
-var mispelled = function(word1, word2) {
-    if (Math.abs(word1.length - word2.length) > 1) return false;
-    let count = 0;
-    for (let i = 0; i < word1.length; i++) {
-        if (word2.includes(word1[i])) count++;
-    }
-    return (Math.abs(word1.length - count) < 2)
-};
---
+++ shortcut.js
@@ -0,0 +1,4 @@
+//https://www.codewars.com/kata/vowel-remover
+function shortcut(string){
+    return string.replace(/[a,e,i,o,u]/g,'');
+}
